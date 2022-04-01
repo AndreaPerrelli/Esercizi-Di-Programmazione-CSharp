09ebc2fc9c90d341327ba8ccfdef537d6d23a481
Andrea <Andrea@DESKTOP-BDOFMVL>
2022-04-01 08:28:28 +0200

    Aggiunti commenti alla soluzione somma-e-media.

--- Esercizi-Di-Programmazione-CSharp/esercizi-stringhe-numeri/somma-e-media.cs
+++ Esercizi-Di-Programmazione-CSharp/esercizi-stringhe-numeri/somma-e-media.cs
@@ -10,30 +10,42 @@
 */
 
 
+
+/*
+  Soluzione scritta da Andrea Antonio Perrelli. © 2022. All rights reserved.
+*/
+
+
 namespace esercizidiprogrammazione_csharp_sommaemedia;
 
 public class SommaEMedia {
 
-    int somma;
-    float media;
-    Dictionary<string, int>? chiaveValoreInsieme { get; set; }
-    bool numeroInseritoNonèValido;
+    int somma; // somma da restituire in output
+    float media; // media da restituire in output
+    Dictionary<string, int>? chiaveValoreInsieme { get; set; } // Dizionario in cui mi salvo coppia {nomenumero - valorenumero}
+    bool numeroInseritoNonèValido; // variabile di controllo per scanner dei numeri.
 
     public void soluzioneSommaEMedia()
     {
-        inizializzaVariabili();
-        leggiNumeri();
-        calcoloSommaEMedia();
+        inizializzaVariabili(); // chiamo il metodo di inizializzazione delle variabili
+        leggiNumeri(); // chiamo il metodo di lettura dei numeri
+        calcoloSommaEMedia(); // chiamo il metodo di calcolo della somma e della media dei numeri letti.
 
     }
 
     private void leggiNumeri()
     {
-        int n;
+        int n; // valore che andrà a contenere il numero letto da console
 
-        if (chiaveValoreInsieme != null)
+        if (chiaveValoreInsieme != null) // mi assicuro che il dizionario chiaveValoreInsieme sia stato inizializzato.
         {
-            foreach (var chiave in chiaveValoreInsieme)
+            /* Tramite il costrutto foreach, per ogni coppia nome-valore nel dizionario chiaveValoreInsieme, vado ad eseguire la lettura del numero 
+               e lo salvo all'internod el dizionario come valore del nome corrispondente.
+               Tramite il costrutto while, Finchè il valore inserito a console dall'utente non è corretto (non è un numero)
+               vado a valorizzare la mia variabile di controllo numeroInseritoNonValido a true, per poi chiamare
+               il metodo stampa che mi permette di mostrare a schermo l'errore e richiedere che venga inserito un valore numerico. 
+            */
+            foreach (var chiave in chiaveValoreInsieme) 
             {
                 Console.WriteLine($"Inserire il numero {chiave.Key} :");
                 while (!int.TryParse(Console.ReadLine(), out n))
@@ -52,25 +64,30 @@ public class SommaEMedia {
 
     private void inizializzaVariabili()
     {
-        somma = 0; media = 0;
-        chiaveValoreInsieme = new Dictionary<string, int>();
+        somma = 0; media = 0; // inizializzo le variabili somma e media assegnando il valore 0 .
+        chiaveValoreInsieme = new Dictionary<string, int>(); // inizializzo il dizionario chiaveValoreInsieme come dizionario vuoto.
         chiaveValoreInsieme.Add("a", 0); chiaveValoreInsieme.Add("b", 0); chiaveValoreInsieme.Add("c", 0); chiaveValoreInsieme.Add("d", 0); chiaveValoreInsieme.Add("e", 0);
+        //vado ad assegnare al dizionario chiaveValoreInsieme dei valori aggiungendo 5 coppie di nomevalore.
     }
 
     private void calcoloSommaEMedia()
     {
-        int indiceMedia = 1;
+        int indiceMedia = 1; //indice con cui mi salvo il valore dei numeri addizionati in modo da sapere per quanto dovrò dividere per avere la media.
         // calcolo somma
+        // controllo che il dizionario chiaveValoreInsieme non sia vuoto, quindi che ci siano i numeri da addizionare.
         if (chiaveValoreInsieme != null)
         {
+            /*Tramite il costrutto foreach, per ogni elemento del dizionario chiaveValoreInsieme, aggiungo il valore del singolo elemento
+              alla variabile somma, e aumento di 1 il valore dei numeri addizionati.
+            */
             foreach (var valore in chiaveValoreInsieme)
             {
-                somma += valore.Value;
+                somma += valore.Value; 
                 indiceMedia++;
             }
-            indiceMedia--;
+            indiceMedia--; // poichè avevo inizializzato indiceMedia ad 1, per avere il corretto valore di numeri addizionati sottraggo il risultato per uno.
 
-            media = somma / indiceMedia;
+            media = somma / indiceMedia; // calcolo la media dividendo la somma per il numero di operatori addizionati.
         }
         stampa("");
     }
